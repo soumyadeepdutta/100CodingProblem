e7cdb9c370d63c7ffff12f94f3e5a11be01714a0
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2020-12-24 18:35:58 +0530

    positive negative

---
+++ 1_positive_negative.cpp
@@ -0,0 +1,14 @@
+// check if a number is positive or negative
+
+#include<iostream>
+int main(void){
+    int num;
+    std::cin>>num;
+    if(num>0){
+        std::cout<<"positive"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"negative"<<std::endl;
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# 100 Coding Problem (Solved)
+
+A little approach to help beginner programmers with 100 most popular coding problem with their solutions in C++
+
+## List of problems
+1. Check if a number is positive or negative
